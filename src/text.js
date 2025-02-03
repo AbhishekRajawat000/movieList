2bf872d13c0e5672316efc11746a4fb21be82034
Abhishek <rajawatsinghabhishek50@gmail.com>
2025-02-03 12:11:03 +0530

    dark mode enabled

---
+++ src/text.js
@@ -0,0 +1,40 @@
+// console.log(1+'2'+3); // 123
+
+// const RollDice = () => {
+//     return Math.floor(Math.random() * 6) + 1;
+//   }
+
+//   console.log(RollDice());
+
+// const countElements = (arr) => {
+//     const count = {};
+  
+//     // Loop through the array and count occurrences of each element
+//     arr.forEach(num => {
+//       count[num] = (count[num] || 0) + 1;
+//     });
+  
+//     // Print the count of each element
+//     for (const key in count) {
+//       console.log(`${key}: ${count[key]}`);
+//     }
+//   };
+  
+//   // Example usage:
+//   const input = [1,5,1,9,3,9,2,3,9,5,8,8,0,1,0,1];
+//   countElements(input);
+  
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
